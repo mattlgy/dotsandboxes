084b2073d83bec654e237c0d5ebdbc06c3a1f82e
Aaron Opfer <me@aaronopfer.com>
2013-09-01 01:14:51 +0000

    Add static file directory

--- application.js
+++ application.js
@@ -1,7 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 
-app.get('/', function (req,res) {
-  res.send('hello world!');
+app.configure(function () {
+	app.use(express.static('static'));
 });
+
 
 app.listen(22863);
